555ce4b81dedff5c2f5cc022e13745946c008a68
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-26 20:42:20 -0800

    shifting works

--- lib/dynamic_array.rb
+++ lib/dynamic_array.rb
@@ -12,28 +12,47 @@ class DynamicArray
   # O(1)
   def [](index)
     raise "index out of bounds" if index >= @length
-
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    @length += 1
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    @store[length] = nil
+    @length -= 1
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if @length == @capacity
+    @store[length] = val
+    @length += 1
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    @store[@length - @length] = nil
+    @length -= 1
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    resize! if @length == @capacity
+    new_store = StaticArray.new(@capacity)
+    new_store[0] = val
+    i = 0
+    while i < @length
+      new_store[1 + i] = @store[i]
+      i+=1
+    end
+    @length += 1
+    @store = new_store
   end
 
   protected
@@ -45,5 +64,12 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    @capacity *= 2
+    new_store = StaticArray.new(@capacity)
+
+    @store.each_with_index do |el,i|
+      new_store[i] = el
+    end
+    @store = new_store
   end
 end
