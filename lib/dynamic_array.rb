7ef0c046af806d3eb3b636c8652b02c79db80c30
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-26 20:51:01 -0800

    dynamic array is done

--- lib/dynamic_array.rb
+++ lib/dynamic_array.rb
@@ -11,7 +11,7 @@ class DynamicArray
 
   # O(1)
   def [](index)
-    raise "index out of bounds" if index >= @length
+    check_index(index)
     @store[index]
   end
 
@@ -23,6 +23,7 @@ class DynamicArray
 
   # O(1)
   def pop
+    raise "index out of bounds" if @length == 0
     @store[length] = nil
     @length -= 1
   end
@@ -37,6 +38,7 @@ class DynamicArray
 
   # O(n): has to shift over all the elements.
   def shift
+    raise "index out of bounds" if @length == 0
     @store[@length - @length] = nil
     @length -= 1
   end
@@ -60,15 +62,17 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
+    raise "index out of bounds" if index == @length
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
     @capacity *= 2
     new_store = StaticArray.new(@capacity)
-
-    @store.each_with_index do |el,i|
-      new_store[i] = el
+    i = 0
+    while i < @length
+      new_store[i] = @store[i]
+      i+=1
     end
     @store = new_store
   end
